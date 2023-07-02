4af33a1fa9da3c74f8f29a9f4badc2999b028835
Timothy Grant <timothygrant@10-248-0-180.wireless.oregonstate.edu>
2023-07-02 14:25:15 -0700

    video feed camera works

--- hand_tracking.py
+++ hand_tracking.py
@@ -1,2 +1,13 @@
 #program followed from https://www.youtube.com/watch?v=01sAkU_NvOY&ab_channel=freeCodeCamp.org
 
+import cv2
+import mediapipe as mp
+import time 
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
