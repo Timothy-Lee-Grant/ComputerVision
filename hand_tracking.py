f9d9dea54d269550bcc6a38962a93eec4e89ec55
Timothy Grant <timothygrant@10-248-0-180.wireless.oregonstate.edu>
2023-07-02 18:09:14 -0700

    successfully implemented hand tracking dots (overlaid onto the video image

--- hand_tracking.py
+++ hand_tracking.py
@@ -6,8 +6,27 @@ import time
 
 cap = cv2.VideoCapture(0)
 
+#he said that this was a formality of setting this up
+mpHands = mp.solutions.hands
+
+#create the hands object
+#we can use default parameters 
+hands = mpHands.Hands()
+
+mpDraw = mp.solutions.drawing_utils
+
 while True:
     success, img = cap.read()
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    #results will contain information about each of the frames which we are currently on 
+    results = hands.process(imgRGB)
+    #print(results.multi_hand_landmarks)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms)
+
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
